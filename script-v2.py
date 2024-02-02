f0ff8fb559d9e492d62ba37de935105503e551da
Mohan Krishna <mohankrishnahb@gmail.com>
2024-02-02 13:07:11 +0530

    seperated config

--- script-v2.py
+++ script-v2.py
@@ -2,55 +2,7 @@ import os
 import pandas as pd
 import sys
 from tqdm import tqdm
-
-CONSTANTS = {
-    "masterFilePath": "source-master.xlsx",
-    "processFolderName": "processed",
-    "reportFolderName": "report",
-    "reportFileName": "report.csv",
-    "fileTypes": [
-        {
-            "fileIdentifier": "SALES_HIST",
-            "sheetName": "Store Sales & Inventory (Hist)"
-        },
-        {
-            "fileIdentifier": "SALES",
-            "sheetName": "Store Sales & Inventory (Weekl)"
-        },
-        {
-            "fileIdentifier": "DC_HIST",
-            "sheetName": "DC Metrics (Hist)"
-        },
-        {
-            "fileIdentifier": "DC",
-            "sheetName": "DC Metrics (Weekly)"
-        },
-        {
-            "fileIdentifier": "MOD",
-            "sheetName": "Modular Plan Metrics"
-        },
-        {
-            "fileIdentifier": "DEMAND_FCST",
-            "sheetName": "Store Demand Forecast"
-        },
-        {
-            "fileIdentifier": "ORDER_FCST",
-            "sheetName": "Order Forecast"
-        },
-        {
-            "fileIdentifier": "MUMD",
-            "sheetName": "Store Markup and Markdowns"
-        },
-        {
-            "fileIdentifier": "ECOMM_SALES",
-            "sheetName": "Ecomm Sales"
-        },
-        {
-            "fileIdentifier": "ECOMM_INV",
-            "sheetName": "Ecomm Inventory"
-        }
-    ]
-}
+from config import CONSTANTS
 
 class TextStyle:
     RED = "\033[91m"
